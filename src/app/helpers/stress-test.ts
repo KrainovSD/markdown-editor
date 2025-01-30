58fb551486805dff7a74ef762c56fa8e873b3d5a
Лосев Денис <denislosev48@gmail.com>
2025-01-30 22:41:09 +0300

    fix: use lock file in action

--- src/app/helpers/stress-test.ts
+++ src/app/helpers/stress-test.ts
@@ -1,3 +1,4 @@
 import { FULL_EXAMPLE } from "./full-example";
 
-export const STRESS_TEST = Array.from({ length: 100 }, () => FULL_EXAMPLE).join("\n");
+export const STRESS_TEST = Array.from({ length: 4000 }, () => FULL_EXAMPLE).join("\n");
+// export const STRESS_TEST = Array.from({ length: 50000 }, () => "### Content").join("\n");
